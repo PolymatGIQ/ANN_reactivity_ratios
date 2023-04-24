15992afe7a29e2435a2941219a6ead7411600aeb
Polymat GIQ <131658083+PolymatGIQ@users.noreply.github.com>
2023-04-24 14:15:38 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,34 @@
+### Parameter configuration
+### ver: 2.0
+### April 24, 2023
+
+# General Parameters
+dire = "."
+r_min = 0.02
+r_max = 20
+bits = 2048
+
+# ANN Parameters
+batches = 16
+epochs = 100
+delta = 0.005
+patience = 20
+r_state = 1
+
+# Figure Parameters
+DPI = 600
+font_s = 10
+font_f = 'sans-serif'
+font_n = 'DejaVu Sans'
+lim = (-0.5, 10.5)
+alph = 0.7
+grc = 'gray'
+grl = 'dashdot'
+grw = 0.5
+pac = 'k'
+pal = 'dashed'
+paw = 1
+Tr_c = 'royalblue'
+Te_c = 'lightgreen'
+tick = [0, 2, 4, 6, 8, 10]
+
